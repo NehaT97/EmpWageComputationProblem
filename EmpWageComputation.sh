0a16eb5bb7a7655e271b12de9e6e7f8b3a88a9d6
unknown <nehathakur764@gmail.com>
2020-10-11 15:11:14 +0530

    Employee wages for Month

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -85,11 +85,48 @@ done
 
 }
 
+
+function Emp_WagesForMonth()
+{
+
+	IS_PART_TIME=1;
+	IS_FULL_TIME=2;
+	MAX_HRS_IN_MONTH=10;
+	EMP_RATE_PER_HR=20;
+	NUM_WORKING_DAYS=20;
+
+	totalEmpHrs=0;
+	totalWorkingDays=0;
+
+	while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+	do
+	((totalWorkingDays++))
+	empCheck=$(($RANDOM%3));
+
+	case $empCheck in
+  		$IS_FULL_TIME)
+      			empHrs=8
+         		;;
+  		$IS_PART_TIME)
+       			empHrs=4
+			;;
+  	*) 
+    			empHrs=0
+   		        ;;
+	esac
+		totalEmpHrs=$(( $totalEmpHrs + $empHrs ));
+	done
+
+		totalWage=$(($totalEmpHrs*$EMP_RATE_PER_HR))
+		echo "Total Wage for month:"$totalWage
+}
+
 function main()
 {
 Emp_Attendance
 Emp_DailyWage
 Emp_PartTimeEmpWage
 Emp_UsingSwitchCases
+Emp_WagesForMonth
 }
 main
