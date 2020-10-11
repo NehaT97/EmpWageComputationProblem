4f399c8715d0cbf6900993d553b42456e15c4917
unknown <nehathakur764@gmail.com>
2020-10-11 14:19:04 +0530

    To check Employee is Present or Absent

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,1 +1,19 @@
 #Welcome to Employee Wage Computation#
+
+function Emp_Attendance()
+{
+	random=$((RANDOM%2))
+	echo $random;
+	if [ $random -eq 1 ]
+	then
+		echo "Employee is Present"
+	else
+		echo "Employee is Absent"
+	fi
+}
+
+function main()
+{
+Emp_Attendance
+}
+main
