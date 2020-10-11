70db7c9718bf3400946d8d7d5e2690b02e545863
unknown <nehathakur764@gmail.com>
2020-10-11 17:25:42 +0530

    GetWorkHours

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -143,6 +143,49 @@ function Emp_TotalWorkingHrsAndDay()
 
 }
 
+function Emp_ToGetWorksHours()
+{
+	IS_PART_TIME=1;
+	IS_FULL_TIME=2;
+	MAX_HRS_IN_MONTH=10;
+	EMP_RATE_PER_HR=20;
+	NUM_WORKING_DAYS=20;
+
+	totalEmpHrs=0;
+	totalWorkingDays=0;
+
+
+	function getWorkHrs(){	
+	local empCheck=$1
+	case $empCheck in
+  	$IS_FULL_TIME)
+      		empHrs=8
+         	;;
+  	$IS_PART_TIME)
+       		empHrs=4
+   		;;
+  *)
+   		 empHrs=0
+        	;;
+	esac
+	echo $empHrs
+}
+
+
+	while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+	do
+	((totalWorkingDays++))
+	empCheck=$(($RANDOM%3));
+	empHrs=$( getWorkHrs $empCheck )
+	totalEmpHrs=$(( $totalEmpHrs + $empHrs ));
+	echo "totalempHrs:"$totalempHrs
+	done
+
+	totalWages=$(($totalEmpHrs*$EMP_RATE_PER_HR))
+	echo :"totalWages:"$totalWages
+}
+
+
 function main()
 {
 Emp_Attendance
@@ -151,5 +194,6 @@ Emp_PartTimeEmpWage
 Emp_UsingSwitchCases
 Emp_WagesForMonth
 Emp_TotalWorkingHrsAndDay
+Emp_ToGetWorksHours
 }
 main
