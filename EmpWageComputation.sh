619f880aa573d257a2ebfdd7573a00f770f12726
unknown <nehathakur764@gmail.com>
2020-10-11 17:45:45 +0530

    Daily Wage Along with total Wage

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -186,6 +186,50 @@ function Emp_ToGetWorksHours()
 }
 
 
+function Emp_WageArray()
+{
+	IS_PART_TIME=1;
+	IS_FULL_TIME=2;
+	MAX_HRS_IN_MONTH=10;
+	EMP_RATE_PER_HR=20;
+	NUM_WORKING_DAYS=20;
+
+
+	totalEmpHrs=0;
+	totalWorkingDays=0;
+
+
+	function getWorkHrs(){
+	local empCheck=$1
+	case $empCheck in
+  		$IS_FULL_TIME)
+      			empHrs=8
+         		;;
+  		$IS_PART_TIME)
+       			empHrs=4
+   			;;
+  	*)
+ 		        empHrs=0
+        		;;
+	esac
+	echo $empHrs
+	}
+
+	count=0;
+	while [[ $totalEmpHrs -lt $MAX_HRS_IN_MONTH && $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+	do
+		((totalWorkingDays++))
+		empCheck=$(($RANDOM%3));
+		empHrs=$( getWorkHrs $empCheck )
+		totalEmpHrs=$(( $totalEmpHrs + $empHrs ));
+		dailyWage[$totalWorkingDays]=$(( $empHrs*$EMP_RATE_PER_HR ))
+		((count++));
+	done	
+		echo $count;
+		totalSalary=$(($totalEmpHrs*$EMP_RATE_PER_HR))
+}
+
+
 function main()
 {
 Emp_Attendance
@@ -195,5 +239,6 @@ Emp_UsingSwitchCases
 Emp_WagesForMonth
 Emp_TotalWorkingHrsAndDay
 Emp_ToGetWorksHours
+Emp_WageArray
 }
 main
